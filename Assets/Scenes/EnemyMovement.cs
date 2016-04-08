368615e335bdaabd72cfb1e8a5b316125d5639e9
AnttiRuu <anttiruuskanen@kamk.fi>
2016-04-08 21:56:14 +0300

    Movement
    Player movement and simple enemy

---
+++ Assets/Scenes/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    GameObject player;
+    public float enemyType;
+    public float speed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyType == 1)
+        {
+            rb.MovePosition(Vector2.MoveTowards(rb.position, player.transform.position, speed * Time.deltaTime));
+        }
+        else if (enemyType == 2)
+        {
+            rb.MovePosition(Vector2.MoveTowards(rb.position, new Vector2(transform.position.x, transform.position.y - 100), speed * Time.deltaTime));
+        }
+        
+    }
+}
---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    float inputX;
+    float inputY;
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+        if (inputX != 0 || inputY != 0)
+        {
+            //rb.transform.position += new Vector3(inputX * speed * Time.deltaTime, inputY * speed * Time.deltaTime);
+            rb.MovePosition(new Vector2(transform.position.x + inputX * speed * Time.deltaTime,transform.position.y +  inputY * speed * Time.deltaTime));
+        }
+        print(rb.velocity);
+    }
+}
