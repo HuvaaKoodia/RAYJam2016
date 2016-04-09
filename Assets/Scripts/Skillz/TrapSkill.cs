5c84a1da747c7b16a2a315862d278df86f2d611e
AnttiRuu <anttiruuskanen@kamk.fi>
2016-04-09 21:25:48 +0300

    Shake fixes
    fix

---
+++ Assets/Scripts/Skillz/TrapSkill.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapSkill : MonoBehaviour
+{
+
+    public Trap TrapPrefab;
+    // Use this for initialization
+    protected override void OnActivate()
+    {
+        Instantiate(TrapPrefab, transform.position, Quaternion.identity);
+    }
+}
